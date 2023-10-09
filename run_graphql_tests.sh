04e8edd3663b406579e675d50c1fc3bace4cac4b
Maya Nordland <mnordland@newclassrooms.org>
2023-10-09 09:05:06 -0700

    Improve test runner script

--- run_graphql_tests.sh
+++ run_graphql_tests.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 
 # uses alpine's default shell, NOT bash.
-
+cd graphql_tests
+shopt -s nullglob
 for gql in *.graphql; do
     graphql_query=`jq -c -n --rawfile query "$gql" '{"query":$query}'`;
     test_file="$(basename "$gql" .graphql)_test.jq";
-    curl -H "Content-Type: application/json" -X POST  -d "$graphql_query" $GRAPHQL_URL | jq -f "$test_file";
+    echo "${test_file}";
+    curl -sS -H "Content-Type: application/json" -X POST  -d "$graphql_query" $GRAPHQL_URL | jq -f "$test_file" || break;
+    echo;
 done
+shopt -u nullglob
+echo;
